b0f89d618c706be6f5e36c87cdaf237e5cd7223f
Sergiy_Tess <sergiyteslenkodeveloper@gmail.com>
2026-02-03 09:55:45 +0300

    . Необхідно створіти список випадкових чисел із випадковою кількістю елементів від 3 до 10.
    2. Створить інший список з 3 елементівзі списку з п.1 - першим, третім і другим з кінця.

--- 3elem_lest.py
+++ 3elem_lest.py
@@ -6,21 +6,21 @@
 
 import random
 rands_num = random.randint(3, 10)
-original = []
+free_list = []
 
-for i in range(rands_num ):
+for i in range(rands_num):
     number = random.randint(1, 100)
-    original.append(number)
+    free_list.append(number)
 
 
-first = original[0]
-second = original[-1]
-third = original[2]
-pre_last = original[-2]
+first = free_list [0]
+second = free_list [-1]
+third = free_list [2]
+pre_last = free_list [-2]
 
 new_rand_list = [first,third, pre_last]
 next_rand_list = [first,second, pre_last]
 
-print("Список 1:", original)
+print("Список 1:", free_list )
 print("Список 2:", new_rand_list)
 print("Список 3:", next_rand_list)
